ff7a7864eb16e6da1fb07736811ce903d4ff1aee
Dhruva Bhat <dhruvabhat24@gmail.com>
2024-01-13 07:30:17 +0530

    Create Day 13: Insertion Sort for Singly Linked List.java

---
+++ January/Day 13: Insertion Sort for Singly Linked List.java
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head)
+    {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        
+        Node temp = head;
+        while(temp!=null){
+            pq.add(temp.data);
+            temp = temp.next;
+        }
+        
+        Node s = new Node(-1);
+        Node ans = s;
+        
+        while(pq.size()!=0){
+            s.next = new Node(pq.remove());
+            s = s.next;
+        }
+        
+        return ans.next;
+    }
+}
