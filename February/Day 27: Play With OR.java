73dac23ce0e8f4130b7a955711e708f4477c1996
Dhruva Bhat <dhruvabhat24@gmail.com>
2024-02-27 08:13:13 +0530

    Create Day 27: Play With OR.java

---
+++ February/Day 27: Play With OR.java
@@ -0,0 +1,8 @@
+class Solution{
+    public static int[] game_with_number (int arr[], int n) {
+        for(int i = 0; i < n-1; i++){
+            arr[i] = arr[i] | arr[i+1];
+        }
+        return arr;
+    }
+}
