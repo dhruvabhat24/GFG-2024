c5305b076eecdac8db35df04119820db463691b8
Dhruva Bhat <dhruvabhat24@gmail.com>
2024-03-13 11:15:19 +0530

    Create Print Matrix in diagonal Pattern.py

---
+++ March /Print Matrix in diagonal Pattern.py
@@ -0,0 +1,44 @@
+class Solution:
+
+    def matrixDiagonally(self, mat):
+
+        n = len(mat)
+
+        i, j = 0, 0
+
+        diagonal_pattern = []
+
+        # The top left half with the major diagonal
+
+        for ij_sum in range(n):
+
+            itr = range(ij_sum + 1)
+
+            if ij_sum % 2 == 0:
+
+                itr = reversed(itr)
+
+            for i in itr:
+
+                diagonal_pattern.append(mat[i][ij_sum - i])
+
+        # The bottom right half
+
+        for ij_sum in range(n, 2 * (n - 1) + 1):
+
+            itr = range(ij_sum - (n - 1), n)
+
+            if ij_sum % 2 == 0:
+
+                itr = reversed(itr)
+
+            for i in itr:
+
+                diagonal_pattern.append(mat[i][ij_sum - i])
+
+        return diagonal_pattern
+
+
+
+
+
