f64de3c8abcca7d383605114ddcfa7afa10ff488
Dhruva Bhat <dhruvabhat24@gmail.com>
2024-04-14 15:52:00 +0530

    Create Day 14: Xoring and Clearing.java

---
+++ April/Day 14: Xoring and Clearing.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void printArr(int n, int arr[]) {
+        // code here
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public void setToZero(int n, int arr[]) {
+        // code here
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=0;
+        }
+    }
+
+    public void xor1ToN(int n, int arr[]) {
+        // code here
+        for(int i=0;i<n;i++)
+        {
+            arr[i]^=i;
+        }
+    }
+}
